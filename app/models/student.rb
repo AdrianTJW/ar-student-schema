a8125d220fcef1ca9bea3682f8a6cab284f45d6c
Adrian Tan <adriantjw23@gmail.com>
2015-12-19 18:05:56 +0800

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,23 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+	validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+	validates :email, :uniqueness => {:case_sensitive => false}
+	validates :age, :numericality => {:greater_than_or_equal_to => 5}
+	
+	def name
+		"#{first_name} #{last_name}"		
+	end
+
+	def age
+		now = Date.today
+		age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+	end
+
+	def phone
+	 phone_num = phone.gsub(/[^\d]/, "")
+	   if phone_num.length < 10
+	       phone_num.errors[:phone] << "invalid phone number."
+	   end
+	end
 end
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,13 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+    	t.string :first_name, length: 64
+    	t.string :last_name, length: 64
+    	t.string :gender, length:64
+    	t.date :birthday
+    	t.string :email, uniqueness: true
+    	t.integer :phone
+    end
   end
 end
